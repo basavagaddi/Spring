c2c549641c45be25296defc0af6d492d3696bcf3
LENOVO <LENOVO@Lenovo-PC>
2019-12-09 23:51:27 +0530

    first commit

---
+++ spring/src/main/java/com/spring/demo/Employee.java
@@ -0,0 +1,19 @@
+package com.spring.demo;
+
+public class Employee {
+	
+	private String userName;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	
+	
+	
+
+}
